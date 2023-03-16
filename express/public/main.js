f7f57f741b059815088cab6609789a79ae1da0f1
Sueun Cho <57546981+sueun-dev@users.noreply.github.com>
2023-03-16 23:54:38 +0900

    Update main.js

--- express/public/main.js
+++ express/public/main.js
@@ -30,7 +30,7 @@ messageInput.addEventListener('keydown', (event) => {
 });
 
 function startChat(roomId) {
-  socket = new WebSocket('ws://localhost:3000');
+  socket = new WebSocket('wss://port-0-chatapp-6g2llfb56c7n.sel3.cloudtype.app');
 
   socket.addEventListener('open', () => {
     onSocketOpen();

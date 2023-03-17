59c2685e8dd2c086d1c800e232951b98b5a97d2a
Sueun Cho <57546981+sueun-dev@users.noreply.github.com>
2023-03-17 18:55:29 +0900

    Update main.js

--- express/public/main.js
+++ express/public/main.js
@@ -4,6 +4,8 @@ const chatArea = document.getElementById('chatArea');
 const messages = document.getElementById('messages');
 const messageInput = document.getElementById('messageInput');
 const sendMessageBtn = document.getElementById('sendMessage');
+const deleteMessageBtn = document.getElementById('deleteMessage');
+const usersOnline = document.getElementById('usersOnline');
 let socket = null;
 
 startChatBtn.addEventListener('click', () => {
@@ -22,6 +24,10 @@ sendMessageBtn.addEventListener('click', () => {
   sendMessage();
 });
 
+deleteMessageBtn.addEventListener('click', () => {
+  deleteMessage();
+});
+
 messageInput.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
     event.preventDefault();
@@ -31,6 +37,7 @@ messageInput.addEventListener('keydown', (event) => {
 
 function startChat(roomId) {
   socket = new WebSocket('wss://port-0-chatapp-6g2llfb56c7n.sel3.cloudtype.app');
+  //socket = new WebSocket('ws://localhost:3000/');
 
   socket.addEventListener('open', () => {
     onSocketOpen();
@@ -62,24 +69,52 @@ function joinRoom(roomId) {
 }
 
 function sendMessage() {
-    const message = messageInput.value.trim();
-    if (message && socket) {
-      socket.send(JSON.stringify({ type: 'message', content: message }));
-      messageInput.value = '';
-    }
+  const message = messageInput.value.trim();
+  if (message && socket) {
+    socket.send(JSON.stringify({ type: 'message', content: message }));
+    messageInput.value = '';
   }
-  
+}
+
+function deleteMessage() {
+  if (messages.lastChild && messages.lastChild.classList.contains('outgoing')) {
+    messages.removeChild(messages.lastChild);
+  }
+}
 
 function onSocketOpen() {
   chatArea.style.display = 'block';
 }
 
+
 function onSocketMessage(event) {
-  const message = event.data;
-  const messageElement = document.createElement('div');
-  messageElement.textContent = message;
-  messages.appendChild(messageElement);
+  const messageData = JSON.parse(event.data);
+  
+  if (messageData.type === 'message') {
+    const messageElement = document.createElement('div');
+    messageElement.classList.add(messageData.senderId === 'self' ? 'outgoing' : 'incoming');
+    messageElement.textContent = messageData.content;
+    messageElement.dataset.messageId = messageData.messageId;
+    messageElement.addEventListener('click', deleteSpecificMessage);
+    messages.appendChild(messageElement);
+  } else if (messageData.type === 'usersOnline') {
+    usersOnline.textContent = `Users online: ${messageData.count}`;
+  } else if (messageData.type === 'delete') {
+    const messageToDelete = document.querySelector(`[data-message-id="${messageData.messageId}"]`);
+    if (messageToDelete) {
+      messages.removeChild(messageToDelete);
+    }
+  }
 }
+
+function deleteSpecificMessage(event) {
+  const messageId = event.target.dataset.messageId;
+  if (socket && messageId) {
+    socket.send(JSON.stringify({ type: 'delete', messageId: messageId }));
+  }
+}
+
+
 
 function onSocketClose(event) {
   stopChat();

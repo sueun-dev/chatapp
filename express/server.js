c9d4b1bcb7b6c16a91a58de985a049bc6d51b054
Sueun Cho <57546981+sueun-dev@users.noreply.github.com>
2023-03-24 00:39:06 +0900

    Update server.js

--- express/server.js
+++ express/server.js
@@ -12,6 +12,9 @@ const wss = new WebSocket.Server({ server });
 app.use(express.static(path.join(__dirname)));
 
 const clients = new Map();
+const activeRooms = new Set();
+
+let currentUsername = null;
 
 wss.on('connection', (socket, req) => {
   console.log('Client connected');
@@ -24,22 +27,38 @@ wss.on('connection', (socket, req) => {
 
   clients.set(clientId, clientInfo);
 
+  //이부분에서 message.username 할때 이름이 사라짐
   socket.on('message', (data) => {
     const message = JSON.parse(data);
+    console.log(currentUsername)
+    currentUsername = message.username
+    console.log('---')
+    console.log(currentUsername)
+    console.log('---')
+    if (message.type === 'leave') {
+      //console.log(`Client ${clientId} left room ${message.roomId}`);
+      updateUsersOnline(clientInfo.roomId);
+      return;
+    }
     if (message.type === 'join') {
       clientInfo.roomId = message.roomId;
-      console.log(`Client ${clientId} joined room ${message.roomId}`);
+      clientInfo.username = currentUsername; // Store the username
+      //console.log(currentUsername)
+      activeRooms.add(clientInfo.roomId);
+      //console.log(`Client ${clientId} joined room ${message.roomId}`);
       updateUsersOnline(clientInfo.roomId);
     } else if (message.type === 'message') {
       const messageId = `${clientId}-${Date.now()}`;
       broadcastMessage(clientId, message.content, messageId);
     } else if (message.type === 'delete') {
       deleteMessage(clientId, message.messageId);
+    } else if (message.type === 'getRooms') {
+      sendRoomsList(clientId);
     }
   });
 
   socket.on('close', () => {
-    console.log('Client disconnected');
+    //console.log('Client disconnected');
     const roomId = clientInfo.roomId;
     clients.delete(clientId);
     updateUsersOnline(roomId);
@@ -53,25 +72,80 @@ function broadcastMessage(senderId, message, messageId) {
     return;
   }
 
+  const username = senderInfo.username; // Get the username from the senderInfo
+
   clients.forEach((clientInfo, clientId) => {
     if (clientInfo.roomId === senderInfo.roomId) {
-      const messageType = (clientId === senderId) ? 'self' : 'other';
-      clientInfo.socket.send(JSON.stringify({ type: 'message', senderId: messageType, content: message, messageId: messageId }));
+      const messageType = clientId === senderId ? 'self' : 'other';
+      //console.log(username)
+      //console.log(message)
+      clientInfo.socket.send(
+        JSON.stringify({
+          type: 'message',
+          senderId: messageType,
+          content: message,
+          messageId: messageId,
+          username: username, // Include the username in the message
+        })
+      );
     }
   });
 }
 
 function updateUsersOnline(roomId) {
-  const usersOnline = Array.from(clients.values()).filter(client => client.roomId === roomId).length;
+  const usersInRoom = Array.from(clients.values())
+    .filter((client) => client.roomId === roomId)
+    .map((client) => client.username);
+
+  const usersOnline = usersInRoom.length;
 
   clients.forEach((clientInfo) => {
     if (clientInfo.roomId === roomId) {
-      clientInfo.socket.send(JSON.stringify({ type: 'usersOnline', count: usersOnline }));
+      clientInfo.socket.send(
+        JSON.stringify({
+          type: 'usersOnline',
+          count: usersOnline,
+          users: usersInRoom,
+        })
+      );
+    }
+  });
+
+  if (usersOnline === 0) {
+    activeRooms.delete(roomId);
+  }
+}
+
+function sendRoomsList(clientId) {
+  const clientInfo = clients.get(clientId);
+
+  if (clientInfo) {
+    clientInfo.socket.send(
+      JSON.stringify({ type: 'roomsList', rooms: Array.from(activeRooms) })
+    );
+  }
+}
+
+function deleteMessage(senderId, messageId) {
+  const senderInfo = clients.get(senderId);
+
+  if (!senderInfo || !senderInfo.roomId) {
+    return;
+  }
+
+  clients.forEach((clientInfo, clientId) => {
+    if (clientInfo.roomId === senderInfo.roomId) {
+      clientInfo.socket.send(
+        JSON.stringify({
+          type: 'delete',
+          messageId: messageId,
+        })
+      );
     }
   });
 }
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  //console.log(`Server listening on port ${PORT}`);
 });

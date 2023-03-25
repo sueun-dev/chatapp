01be0ad1888b18a0d8ac2138468a1b6bc61a8f2f
Sueun Cho <57546981+sueun-dev@users.noreply.github.com>
2023-03-26 01:13:42 +0900

    Update socketHandler.js

--- express/public/socketHandler.js
+++ express/public/socketHandler.js
@@ -1,13 +1,13 @@
-
-
 let socket = null;
 let currentRoomId = null; // Add this line
 let currentUsername = null;
 
 //웹 시작할때 실행
 function startChat(roomId) {
-  socket = new WebSocket('wss://port-0-chatapp-6g2llfb56c7n.sel3.cloudtype.app');
-  //socket = new WebSocket('ws://localhost:3000/');
+  //socket server 지정
+  //ws 는 http, wss 는 https
+  //socket = new WebSocket('wss://port-0-chatapp-6g2llfb56c7n.sel3.cloudtype.app');
+  socket = new WebSocket('ws://localhost:3000/');
 
   socket.addEventListener('open', () => {
     onSocketOpen();
@@ -15,12 +15,16 @@ function startChat(roomId) {
     const username = document.getElementById('username').value.trim(); // Get the username from the input field
     //정상작동
     currentUsername = username;
-    joinRoom(roomId, currentUsername); // Pass the username when joining the room
-    socket.send(JSON.stringify({ type: 'getRooms' })); // Request the list of active chat rooms
 
+
+  const isPrivate = document.getElementById("privateRoom").checked; // Get the private checkbox value
+  joinRoom(roomId, currentUsername, isPrivate); // Pass the isPrivate value to joinRoom
+    //위에 체크
+  socket.send(JSON.stringify({ type: 'getRooms' })); // Request the list of active chat rooms
+
+    //You are in X 
     updateRoomNumber(roomId);
   });
-
   socket.addEventListener('message', onSocketMessage);
   socket.addEventListener('close', onSocketClose);
   socket.addEventListener('error', onSocketError);
@@ -52,6 +56,7 @@ function stopChat() {
     socket.close();
   }
 
+  //버튼 disabled
   startChatBtn.disabled = false;
   stopChatBtn.disabled = true;
   chatArea.style.display = 'none';
@@ -59,22 +64,37 @@ function stopChat() {
 }
 
 //이게 위에 
-function joinRoom(roomId, currentUsername) {
+function joinRoom(roomId, currentUsername, isPrivate) {
   if (socket) {
+    if (currentRoomId) {
+      socket.send(JSON.stringify({ type: "leave", roomId: currentRoomId }));
+    }
+
     updateRoomNumber(roomId);
-    socket.send(JSON.stringify({ type: 'leave', roomId: currentRoomId })); // Leave the previous room
-    socket.send(JSON.stringify({ type: 'join', roomId: roomId, username: currentUsername }));
-    currentRoomId = roomId; // Update the currentRoomId
+    socket.send(
+      JSON.stringify({
+        type: "join",
+        roomId: roomId,
+        username: currentUsername,
+        isPrivate: isPrivate, // Send the isPrivate flag
+      })
+    );
+    currentRoomId = roomId;
   }
 }
 
-//여기는 채팅창 변경해도 username 유지됌
+
+
+//여기는 채팅창 변경해도 username 유지됨
 function sendMessage() {
   const message = messageInput.value.trim();
+  //message는 내가 입력한 값이 나오게 됨
   if (message && socket) {
     socket.send(
+      //{"type":"message","content":"eryg","username":"123"} < 이런식으로 저장
       JSON.stringify({ type: 'message', content: message, username: currentUsername })
     ); // Send the username with the message
+    console.log("chat2")
     messageInput.value = '';
   }
 }
@@ -91,21 +111,25 @@ function onSocketOpen() {
   chatArea.style.display = 'block';
 }
 
-//이부분에서 닉네임 소멸
+// user name 잘 작동됌
 function onSocketMessage(event) {
   const messageData = JSON.parse(event.data);
-  console.log(messageData);
-
   switch (messageData.type) {
+    //메세지 보낼때만 작동
     case 'message':
+      console.log('chat3')
       handleMessage(messageData);
       break;
+    //Start Chat, 과 위 방 번호 눌렀을때 작동
     case 'usersOnline':
+
       handleUsersOnline(messageData);
       break;
+    //드레그해서 지울때만 작동 
     case 'delete':
       handleDelete(messageData);
       break;
+    //Start Chat 버튼 1번 Refresh Rooms List 누를때만 작동
     case 'roomsList':
       handleRoomsList(messageData);
       break;
@@ -128,10 +152,22 @@ function handleMessage(messageData) {
   const contentElement = document.createElement('span');
   contentElement.textContent = messageData.content;
   messageElement.appendChild(contentElement);
+
+  // Add a delete button for each message
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'X';
+  deleteBtn.classList.add('delete-btn');
+  deleteBtn.addEventListener('click', () => {
+    deleteSpecificMessage(messageData.messageId);
+  });
+  
+  messageElement.appendChild(deleteBtn);
+
   messageElement.dataset.messageId = messageData.messageId;
-  messageElement.addEventListener('click', deleteSpecificMessage);
   messages.appendChild(messageElement);
+  scrollToBottom();
 }
+
 
 //실시간 작동
 function handleUsersOnline(messageData) {
@@ -151,12 +187,12 @@ function handleRoomsList(messageData) {
   updateRoomsList(messageData.rooms);
 }
 
-function deleteSpecificMessage(event) {
-  const messageId = event.target.dataset.messageId;
+function deleteSpecificMessage(messageId) {
   if (socket && messageId) {
     socket.send(JSON.stringify({ type: 'delete', messageId: messageId }));
   }
 }
+
 
 function onSocketClose() {
   stopChat();

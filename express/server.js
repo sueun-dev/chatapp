b2026a391263459d964484a81743816006f6adb8
Sueun Cho <57546981+sueun-dev@users.noreply.github.com>
2023-03-26 01:14:07 +0900

    Update server.js

--- express/server.js
+++ express/server.js
@@ -3,22 +3,25 @@ const http = require('http');
 const WebSocket = require('ws');
 const path = require('path');
 
+//Set up the Express app
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Create an HTTP server and WebSocket server
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 app.use(express.static(path.join(__dirname)));
 
+// Store clients and active rooms
 const clients = new Map();
 const activeRooms = new Set();
 
 let currentUsername = null;
 
 wss.on('connection', (socket, req) => {
-  console.log('Client connected');
-
+  isPrivate: false
+  // Assign a unique ID for each client
   const clientId = Date.now();
   const clientInfo = {
     socket: socket,
@@ -27,25 +30,24 @@ wss.on('connection', (socket, req) => {
 
   clients.set(clientId, clientInfo);
 
-  //이부분에서 message.username 할때 이름이 사라짐
+  //방을 변경해도 currentUsername은 유지, priavte 버튼과 stop chat 버튼만 작동 안함. 다른 버튼은 모두 여길 통해서 작동됨
   socket.on('message', (data) => {
     const message = JSON.parse(data);
-    console.log(currentUsername)
     currentUsername = message.username
-    console.log('---')
-    console.log(currentUsername)
-    console.log('---')
     if (message.type === 'leave') {
-      //console.log(`Client ${clientId} left room ${message.roomId}`);
-      updateUsersOnline(clientInfo.roomId);
+      const roomId = clientInfo.roomId;
+      clientInfo.roomId = null; // Set the client's roomId to null
+      // 이부분이 실시간으로 방에  Users online 을 변경해주는곳
+      updateUsersOnline(roomId);
       return;
     }
-    if (message.type === 'join') {
+    if (message.type === "join") {
+      checkEmptyRooms();
       clientInfo.roomId = message.roomId;
-      clientInfo.username = currentUsername; // Store the username
-      //console.log(currentUsername)
+      clientInfo.username = message.username;
+      clientInfo.isPrivate = message.isPrivate; // Store the private flag
       activeRooms.add(clientInfo.roomId);
-      //console.log(`Client ${clientId} joined room ${message.roomId}`);
+      //추가작동
       updateUsersOnline(clientInfo.roomId);
     } else if (message.type === 'message') {
       const messageId = `${clientId}-${Date.now()}`;
@@ -53,21 +55,29 @@ wss.on('connection', (socket, req) => {
     } else if (message.type === 'delete') {
       deleteMessage(clientId, message.messageId);
     } else if (message.type === 'getRooms') {
+      checkEmptyRooms();
       sendRoomsList(clientId);
     }
   });
 
-  socket.on('close', () => {
-    //console.log('Client disconnected');
-    const roomId = clientInfo.roomId;
-    clients.delete(clientId);
-    updateUsersOnline(roomId);
-  });
+// Update the 'close' event listener
+// Add these lines at the beginning of the 'close' event listener
+socket.on('close', () => {
+  const roomId = clientInfo.roomId;
+  clients.delete(clientId);
+
+  if (roomId) {
+    updateUsersOnline(roomId); // Update the users online count when a user leaves the chat room or closes the webpage
+  }
+
+  checkEmptyRooms(); // Add this line to check for empty rooms when a client disconnects
 });
+
+});
+
 
 function broadcastMessage(senderId, message, messageId) {
   const senderInfo = clients.get(senderId);
-
   if (!senderInfo || !senderInfo.roomId) {
     return;
   }
@@ -110,18 +120,36 @@ function updateUsersOnline(roomId) {
       );
     }
   });
-
-  if (usersOnline === 0) {
-    activeRooms.delete(roomId);
-  }
 }
 
+
+// Add this function
+function checkEmptyRooms() {
+  activeRooms.forEach((roomId) => {
+    const usersInRoom = Array.from(clients.values()).filter(
+      (client) => client.roomId === roomId
+    );
+
+    if (usersInRoom.length === 0) {
+      activeRooms.delete(roomId);
+    }
+  });
+}
+
+// Update the sendRoomsList function to filter private rooms:
 function sendRoomsList(clientId) {
   const clientInfo = clients.get(clientId);
 
   if (clientInfo) {
+    const publicRooms = Array.from(activeRooms).filter((roomId) => {
+      const roomClients = Array.from(clients.values()).filter(
+        (client) => client.roomId === roomId
+      );
+      return !roomClients.some((client) => client.isPrivate);
+    });
+
     clientInfo.socket.send(
-      JSON.stringify({ type: 'roomsList', rooms: Array.from(activeRooms) })
+      JSON.stringify({ type: "roomsList", rooms: publicRooms })
     );
   }
 }
@@ -149,3 +177,4 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   //console.log(`Server listening on port ${PORT}`);
 });
+
